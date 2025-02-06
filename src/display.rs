4d6d8baa2987d884f4aa8d75d900efd2d88e85b5
Juan Esteban Guevara Roncancio <juguevara@unal.edu.co>
2025-02-06 10:28:46 -0500

    feat(src/display.rs,-src/tasks/display_task.rs): Updated Display to use Asynchronous implementation

--- src/display.rs
+++ src/display.rs
@@ -1,26 +1,25 @@
-use core::cell::RefCell;
-use embassy_embedded_hal::shared_bus::blocking::spi::SpiDevice;
+use embassy_embedded_hal::shared_bus::asynch::spi::SpiDevice;
 use embassy_stm32::{
     gpio::{Level, Output, Pin, Speed},
     mode::Async,
     spi::Spi,
 };
-use embassy_sync::blocking_mutex::{raw::NoopRawMutex, NoopMutex};
-use mipidsi::{interface::SpiInterface, models::ILI9341Rgb565, Display};
+use embassy_sync::{blocking_mutex::raw::NoopRawMutex, mutex::Mutex};
+use mipidsi::{interface::SpiInterfaceAsync, models::ILI9341Rgb565, Display};
 use static_cell::StaticCell;
 
-type SpiBus = NoopMutex<RefCell<Spi<'static, embassy_stm32::mode::Async>>>;
+type SpiBus = Mutex<NoopRawMutex, Spi<'static, embassy_stm32::mode::Async>>;
 
-pub fn create_display<'a, P1: Pin, P2: Pin, P3: Pin>(
+pub async fn create_display<'a, P1: Pin, P2: Pin, P3: Pin>(
     spi: Spi<'static, Async>,
     reset_pin: P1,
     cs_pin: P2,
     dc_pin: P3,
     buffer: &'a mut [u8],
 ) -> Display<
-    SpiInterface<
+    SpiInterfaceAsync<
         'a,
-        embassy_embedded_hal::shared_bus::blocking::spi::SpiDevice<
+        embassy_embedded_hal::shared_bus::asynch::spi::SpiDevice<
             'static,
             NoopRawMutex,
             embassy_stm32::spi::Spi<'static, embassy_stm32::mode::Async>,
@@ -32,7 +31,7 @@ pub fn create_display<'a, P1: Pin, P2: Pin, P3: Pin>(
     embassy_stm32::gpio::Output<'static>,
 > {
     static SPI_BUS: StaticCell<SpiBus> = StaticCell::new();
-    let spi_bus = SPI_BUS.init(NoopMutex::new(RefCell::new(spi)));
+    let spi_bus = SPI_BUS.init(Mutex::new(spi));
 
     let reset = Output::new(reset_pin, Level::Low, Speed::VeryHigh);
     let cs = Output::new(cs_pin, Level::High, Speed::VeryHigh);
@@ -41,9 +40,11 @@ pub fn create_display<'a, P1: Pin, P2: Pin, P3: Pin>(
     let dev = SpiDevice::new(spi_bus, cs);
     let mut delay = embassy_time::Delay;
 
-    let interface = mipidsi::interface::SpiInterface::new(dev, dc, buffer);
+    let interface = SpiInterfaceAsync::new(dev, dc, buffer);
+
     mipidsi::Builder::new(ILI9341Rgb565, interface)
         .reset_pin(reset)
         .init(&mut delay)
+        .await
         .unwrap()
 }
--- src/tasks/display_task.rs
+++ src/tasks/display_task.rs
@@ -1,9 +1,11 @@
 use crate::display;
 use embassy_stm32::{gpio::AnyPin, mode::Async, spi::Spi};
 use embassy_time::{Duration, Ticker};
-use embedded_graphics::{pixelcolor::Rgb565, prelude::*};
+use embedded_graphics::{pixelcolor::Rgb565, prelude::*, primitives::Rectangle};
 
-const GOAL_FPS: u64 = 10;
+const GOAL_FPS: u64 = 30;
+const BUFFER_COLUMN_SIZE: usize = 80;
+const SCREEN_WIDTH: usize = 320;
 
 #[embassy_executor::task]
 pub async fn display_task(
@@ -13,19 +15,30 @@ pub async fn display_task(
     dc_pin: AnyPin,
 ) {
     let mut ticker = Ticker::every(Duration::from_millis(1000 / GOAL_FPS));
-    let mut buffer = [0_u8; 240];
+    let mut buffer = [0_u8; 2 * 240 * BUFFER_COLUMN_SIZE];
     let mut display =
-        display::create_display(spi, reset_pin, cs_pin, dc_pin, buffer.as_mut_slice());
+        display::create_display(spi, reset_pin, cs_pin, dc_pin, buffer.as_mut_slice()).await;
     let mut color = Rgb565::BLACK;
 
+    let mut counter = 0;
     loop {
-        display.clear(color).unwrap();
-        ticker.next().await;
-
-        if color == Rgb565::BLACK {
-            color = Rgb565::WHITE;
-        } else {
-            color = Rgb565::BLACK;
+        for x in 0..=(SCREEN_WIDTH / BUFFER_COLUMN_SIZE) {
+            let area = Rectangle::new(
+                Point::new(0, (x * BUFFER_COLUMN_SIZE) as i32),
+                Size::new(240, BUFFER_COLUMN_SIZE as u32),
+            );
+            display.fill_solid(&area, color).await.unwrap();
+            display.flush().await.unwrap();
         }
+
+        if counter % GOAL_FPS == 0 {
+            if color == Rgb565::BLACK {
+                color = Rgb565::WHITE;
+            } else {
+                color = Rgb565::BLACK;
+            }
+        }
+        counter = (counter + 1) % (GOAL_FPS * 2);
+        ticker.next().await;
     }
 }
