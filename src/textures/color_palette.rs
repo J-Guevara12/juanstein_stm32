baac33b0182c22ab1ea6c70e71a39a70b1c973d9
Juan Esteban Guevara Roncancio <juguevara@unal.edu.co>
2025-03-03 10:57:16 -0500

    feat(src/textures/): Added new textures (Hazard wall and door)

--- src/textures/color_palette.rs
+++ src/textures/color_palette.rs
@@ -3,13 +3,15 @@ use embedded_graphics::{
     prelude::{RgbColor, WebColors},
 };
 
-const PALETTE_LEN: usize = 4;
+const PALETTE_LEN: usize = 6;
 
 pub static COLOR_PALETTE_V: [Rgb565; PALETTE_LEN] = [
     Rgb565::CSS_FIRE_BRICK,
     Rgb565::CSS_SLATE_GRAY,
     Rgb565::CSS_BLACK,
     Rgb565::YELLOW,
+    Rgb565::new(15, 7, 0),
+    Rgb565::new(16, 12, 0),
 ];
 
 pub static COLOR_PALETTE_H: [Rgb565; PALETTE_LEN] = [
@@ -17,4 +19,6 @@ pub static COLOR_PALETTE_H: [Rgb565; PALETTE_LEN] = [
     Rgb565::new(9, 23, 11),
     Rgb565::CSS_BLACK,
     Rgb565::new(26, 26, 0),
+    Rgb565::new(15, 7, 0),
+    Rgb565::new(13, 10, 0),
 ];
--- src/textures/mod.rs
+++ src/textures/mod.rs
@@ -1,11 +1,18 @@
 mod all_black;
 mod color_palette;
+mod door_1;
 mod wall_1;
 mod wall_2;
+mod wall_3;
 
 pub const TEXTURE_SIZE: usize = 20;
 pub use color_palette::COLOR_PALETTE_H;
 pub use color_palette::COLOR_PALETTE_V;
 
-pub const TEXTURES: [[[u8; TEXTURE_SIZE]; TEXTURE_SIZE]; 3] =
-    [all_black::TEXTURE, wall_1::TEXTURE, wall_2::TEXTURE];
+pub const TEXTURES: [[[u8; TEXTURE_SIZE]; TEXTURE_SIZE]; 5] = [
+    all_black::TEXTURE,
+    wall_1::TEXTURE,
+    wall_2::TEXTURE,
+    wall_3::TEXTURE,
+    door_1::TEXTURE,
+];
