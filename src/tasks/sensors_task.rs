c5ca3a196e8e9a45688e675efc69198869c2ecc3
Juan Esteban Guevara Roncancio <juguevara@unal.edu.co>
2025-03-03 10:59:31 -0500

    feat(sensor_task.rs): Updated ADC Resolution

--- src/tasks/sensors_task.rs
+++ src/tasks/sensors_task.rs
@@ -1,4 +1,4 @@
-use defmt::{error, info};
+use defmt::error;
 use embassy_stm32::adc::AdcChannel;
 use embassy_stm32::i2c::I2c;
 use embassy_stm32::mode::Async;
@@ -12,8 +12,11 @@ const GOAL_SAMPLE_FREQUENCY_HZ: u64 = 40;
 
 #[embassy_executor::task]
 pub async fn sensors_task(adc: ADC1, channel_x: PC0, channel_y: PC1, i2c: I2c<'static, Async>) {
+    let mut ticker = Ticker::every(Duration::from_millis(1000 / GOAL_SAMPLE_FREQUENCY_HZ));
+
     let mut adc = init(adc);
-    adc.set_resolution(embassy_stm32::adc::Resolution::BITS12);
+    adc.set_resolution(embassy_stm32::adc::Resolution::BITS6);
+
     let mut channel_x = channel_x.degrade_adc();
     let mut channel_y = channel_y.degrade_adc();
 
@@ -22,18 +25,18 @@ pub async fn sensors_task(adc: ADC1, channel_x: PC0, channel_y: PC1, i2c: I2c<'s
         mpu6050::device::AccelRange::G2,
         mpu6050::device::GyroRange::D250,
     );
+
     mpu.init(&mut embassy_time::Delay);
 
-    let mut ticker = Ticker::every(Duration::from_millis(1000 / GOAL_SAMPLE_FREQUENCY_HZ));
     let gpioc_ascr = 0x4800082C as *mut u32;
     unsafe { *gpioc_ascr = 0x3 }
 
     loop {
         {
-            let x = ((adc.blocking_read(&mut channel_x) >> 8) as i32 - 7) as f32
-                / (GOAL_SAMPLE_FREQUENCY_HZ as f32 * 16.0);
-            let y = ((adc.blocking_read(&mut channel_y) >> 8) as i32 - 7) as f32
-                / (GOAL_SAMPLE_FREQUENCY_HZ as f32 * 16.0);
+            let x = ((adc.blocking_read(&mut channel_x)) as i32 - 31) as f32
+                / (GOAL_SAMPLE_FREQUENCY_HZ as f32 * 64.0);
+            let y = ((adc.blocking_read(&mut channel_y)) as i32 - 31) as f32
+                / (GOAL_SAMPLE_FREQUENCY_HZ as f32 * 64.0);
             let mut player = PLAYER.lock().await;
             player.move_player(y, -x);
             match mpu.get_gyro() {
